3a846d4a31ea6bd6b0fa31295a19462182cad4db
Max Plomer <maxplomer@gmail.com>
2016-04-10 14:16:28 -0400

    Overwrite head method to allow api calls from different origins

--- app/main/controllers/trades_controller.rb
+++ app/main/controllers/trades_controller.rb
@@ -10,5 +10,15 @@ module Main
       store._trades << data
       render json: data
     end
+
+    private
+
+    # Overwrite head method to allow api calls from different origins
+    def head(status, additional_headers = {})
+      @response_status = status
+      response_headers.merge!(additional_headers)
+      response_headers.merge!({"Access-Control-Allow-Origin": "*"})
+    end
+
   end
 end
