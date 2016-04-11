5333c9ebfb6cc1da681d42fceeb83d3d0b515634
Max Plomer <maxplomer@gmail.com>
2016-04-11 19:28:13 -0400

    Comment out default routes

--- app/main/config/routes.rb
+++ app/main/config/routes.rb
@@ -3,15 +3,15 @@
 get "/api/trades", controller: 'trades', action: 'index'
 post "/api/trades", controller: 'trades', action: 'create'
 
-client '/about', action: 'about'
+# client '/about', action: 'about'
 
 # Routes for login and signup, provided by user_templates component gem
-client '/signup', component: 'user_templates', controller: 'signup'
-client '/login', component: 'user_templates', controller: 'login', action: 'index'
-client '/password_reset', component: 'user_templates', controller: 'password_reset', action: 'index'
-client '/forgot', component: 'user_templates', controller: 'login', action: 'forgot'
-client '/account', component: 'user_templates', controller: 'account', action: 'index'
+# client '/signup', component: 'user_templates', controller: 'signup'
+# client '/login', component: 'user_templates', controller: 'login', action: 'index'
+# client '/password_reset', component: 'user_templates', controller: 'password_reset', action: 'index'
+# client '/forgot', component: 'user_templates', controller: 'login', action: 'forgot'
+# client '/account', component: 'user_templates', controller: 'account', action: 'index'
 
 # The main route, this should be last. It will match any params not
 # previously matched.
-client '/', {}
+# client '/', {}
