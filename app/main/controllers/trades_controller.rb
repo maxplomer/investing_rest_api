c790f121b6dd7e6e51ad10d62d971cd83a0544d2
Max Plomer <maxplomer@gmail.com>
2016-05-02 01:09:28 -0400

    authenticate user for my trades route

--- app/main/controllers/trades_controller.rb
+++ app/main/controllers/trades_controller.rb
@@ -17,15 +17,19 @@ module Main
       id_token = params._idToken
       user_id = params._id
 
-      # Only want the 10 most recent
-      store._trades.where(user_id: user_id).length.then do |trades_length|
-        if trades_length > 10
-          data = store._trades.where(user_id: user_id).skip(trades_length - 10).limit(10).all
-        else
-          data = store._trades.where(user_id: user_id).all
-        end
+      AuthTasks.authenticate_user(user_id, id_token).then do
+        # Only want the 10 most recent
+        store._trades.where(user_id: user_id).length.then do |trades_length|
+          if trades_length > 10
+            data = store._trades.where(user_id: user_id).skip(trades_length - 10).limit(10).all
+          else
+            data = store._trades.where(user_id: user_id).all
+          end
 
-        render json: data
+          render json: data
+        end
+      end.fail do |error|
+        puts "Error: #{error}"
       end
     end
 
